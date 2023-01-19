8c4db6d93f984447314808f12019875aca956ef0
compawo <pundorkamila@gmail.com>
2023-01-19 16:32:36 +0100

    simpsons

---
+++ src/components/Simpson/Simpson.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Simpson = (props) => {
+    const {name, surname, image} = props;
+    return (
+        <div>
+            <div>name: {name}</div>
+            <div>surname: {surname}</div>
+            <img src={image} alt={name}/>
+        </div>
+    );
+};
+
+export {Simpson};
