a230c53c8daf3a2cc10b624744ebee338b3001f9
compawo <pundorkamila@gmail.com>
2023-01-21 15:56:02 +0100

    spaceX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+    const [launches, setLaunches] = useState([])
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/').then(resp => resp.json().then(resp => setLaunches(resp)))
+    }, [])
 
-  return (
-      <div>
-        app
-      </div>
-  );
+    return (
+        <div>
+            {launches.filter(item => item.launch_year !== '2020').map(item => {
+                return (
+                    <div key={item.flight_number}>
+                        {item.mission_name}: - {item.launch_year}
+                    </div>
+                )
+            })}
+        </div>
+    );
 };
 
 export {App};
