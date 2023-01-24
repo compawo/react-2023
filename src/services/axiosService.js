831406fa3e38e32ad73240b9203f6cc954a7e293
compawo <pundorkamila@gmail.com>
2023-01-24 17:33:17 +0100

    export

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {baseUrl} from "../configs";
 
-const axiosService = axios.create({baseUrl})
+const axiosService = axios.create({baseUrl});
 
 export {
     axiosService
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,7 @@ import {urls} from "../configs";
 const userService = {
     getAll: () => axiosService.get(urls.users),
     create: (newUser) => axiosService.post(urls.users, newUser),
+    updateById: (id, data) => axiosService.put(`${urls.users}/${id}`,data)
    }
 
 
